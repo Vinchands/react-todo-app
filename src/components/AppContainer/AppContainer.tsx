f9194840156e6dd671bf6cb07de0f53bb4539f84
Kevin CS <kevinchandra031@gmail.com>
2024-12-11 08:45:32 +0700

    Fix search bar, modify create form.

--- src/components/AppContainer/AppContainer.tsx
+++ src/components/AppContainer/AppContainer.tsx
@@ -9,9 +9,11 @@ export default function AppContainer({ children }: AppContainerProps) {
     return (
         <section className='app-container group'>
             { children }
-            <a href='https://github.com/Vinchands' className='block text-center text-slate-400 py-2' target='_blank' rel='noopener noreferrer'>
-                <i className='bi bi-github'></i> { new Date().getFullYear() } Vinchands
-            </a>
+            <div className='p-2'>
+                <a href='https://github.com/Vinchands' className='block text-center text-slate-400' target='_blank' rel='noopener noreferrer'>
+                    <i className='bi bi-github'></i> { new Date().getFullYear() } Vinchands
+                </a>
+            </div>
         </section>
     )
 }
--- src/components/CreateTodoForm/CreateTodoForm.tsx
+++ src/components/CreateTodoForm/CreateTodoForm.tsx
@@ -29,28 +29,32 @@ export default function CreateTodoForm() {
     }
     
     return (
-        <div className='flex gap-1 py-3'>
-            <input 
-                type='text' 
-                className='grow rounded-md p-2 focus:outline-none dark:text-white dark:bg-slate-700' 
-                value={ text } 
-                onChange={ handleChange } 
-                onKeyDown={ 
-                    (e: React.KeyboardEvent<HTMLInputElement>) => {
-                        if (e.key === 'Enter' && text.trim()) handleCreate()
-                    }
-                } 
-                placeholder='Add todo...'
-                disabled={ loading } />
-            {
-                text &&
-                <button 
-                    className='font-extrabold bg-fuchsia-300 rounded-md p-2 transition-colors ease-linear duration-100 hover:bg-fuchsia-500 dark:bg-sky-600 dark:hover:bg-sky-700'
-                    onClick={ handleCreate }
-                    disabled={ loading }>
-                    <i className='bi bi-plus-lg'></i>
-                </button>
-            }
-        </div>
+        <>
+            <div className='flex gap-1 py-3'>
+                <input 
+                    type='text' 
+                    className='grow rounded-md p-2 focus:outline-none dark:text-white dark:bg-slate-700' 
+                    value={ text } 
+                    maxLength={ 255 }
+                    onChange={ handleChange } 
+                    onKeyDown={ 
+                        (e: React.KeyboardEvent<HTMLInputElement>) => {
+                            if (e.key === 'Enter' && text.trim()) handleCreate()
+                        }
+                    } 
+                    placeholder='Add todo...'
+                    disabled={ loading } />
+                {
+                    text &&
+                    <button 
+                        className='font-extrabold bg-fuchsia-300 rounded-md p-2 transition-colors ease-linear duration-100 hover:bg-fuchsia-500 dark:bg-sky-600 dark:hover:bg-sky-700'
+                        onClick={ handleCreate }
+                        disabled={ loading }>
+                        <i className='bi bi-plus-lg'></i>
+                    </button>
+                }
+            </div>
+            { text.length > 0 && <span className='block sm:inline text-xs px-2'>{text.length}/255</span> }
+        </>
     )
 }
